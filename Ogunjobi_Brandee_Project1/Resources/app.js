eb41d5afdca58c5ec7ad51fb7047ac382d55d8d3
brandeeogunjobi <ketaloginc@brandees-MacBook-Pro.local>
2014-12-02 19:29:21 -0700

    commiting all changes

--- Ogunjobi_Brandee_Project1/Resources/app.js
+++ Ogunjobi_Brandee_Project1/Resources/app.js
@@ -1,17 +1,18 @@
 //Set Background Color
 Ti.UI.setBackgroundColor('#999');
 
-var marketing = ['Audience Engagement', 'Web Analytics','Real Time Reporting', 'Web Design', "Mobile Apps"];
+var marketingItems = ["1. Web Analytics", "2. Real Time Reporting", "3. Audience Engagement", "4. Web Design", "5. Mobile Apps"];
+
 
 
 var window1 = Ti.UI.createWindow ({
 	backgroundColor: "#fff",
 	backgroundImage:"vector-grey-abstract-background-for-design.png",
-	backgroundRepeat: true,
-	title: "Ketalog Inc."
+	backgroundRepeat: true
+
 });
 
-var myFirstView = Ti.UI.createView({
+var mainView = Ti.UI.createView({
 	backgroundColor: "#333",
 	height: 150,
 	top: 120,
@@ -20,25 +21,42 @@ var myFirstView = Ti.UI.createView({
 	borderRadius: 5
 });
 
-var myText = Ti.UI.createLabel({
-	text: "Hello World",
+var myIntro = Ti.UI.createLabel({
+	text: "Ketalog List Of Services",
 	color: "#fff",
-	font: {fontSize: 24, fontFamily: "Helvetica", fontWeight: "bold", fontStyle: "italic"},
+	font: {fontSize: 14, fontFamily: "Helvetica", fontWeight: "bold", fontStyle: "italic"},
 	textAlign: 'center',
 	top: 60,
 	left: 70
 });
 
-var buttonView = Ti.UI.createLabel({
+var buttonNext = Ti.UI.createView({
 	backgroundColor: '#8E2899',
-	top: myFirstView.top + myFirstView.height + 80,
+	top: 69,
 	left: 20,
 	right: 20,
 	borderRadius: 5,
 	height: 50
 });
 
-var buttonText = Ti.UI.createLabel({
+var nextLabel = Ti.UI.createLabel({
+	color: '#fff',
+	font: {fontSize: 16, fontFamily: 'Arial'},
+	textAlign: "center",
+	text: 'Next'
+	
+});
+
+var buttonPrevious = Ti.UI.createView({
+	backgroundColor: '#8E2899',
+	top: mainView.top + mainView.height + 1,
+	left: 20,
+	right: 20,
+	borderRadius: 5,
+	height: 50
+});
+
+var previousLabel = Ti.UI.createLabel({
 	color: '#fff',
 	font: {fontSize: 16, fontFamily: 'Arial'},
 	textAlign: "center",
@@ -46,46 +64,38 @@ var buttonText = Ti.UI.createLabel({
 	
 });
 
-var buttonView2 = Ti.UI.createLabel({
-	backgroundColor: '#8E2899',
-	top: myFirstView.top + myFirstView.height + 150,
-	left: 20,
-	right: 20,
-	borderRadius: 5,
-	height: 50
-});
-
-var buttonText2 = Ti.UI.createLabel({
-	color: '#fff',
-	font: {fontSize: 14, fontFamily: 'Arial'},
-	textAlign: "center",
-	text: 'Next'
-	
-});
-
-var marketingItems = function(){
-	myFirstView.hide();
-	myFirstView = null;
-	myText = null;
-	
-var marketingLabel = Ti.UI.createLabel({
-	text: marketing[0],
-	backgroundColor: '#fff',
-	left: 20,
-	right: 20,
-	top: 20,
-	borderRadius: 5
-});
 
 
-window1.add(marketingLabel);
+var length = marketingItems.length;
+var i = -1;
+
+var newbuttonNext = function(){
+	if(i === length -1){
+		i = -1;
+}; i++; myIntro.text = marketingItems[i];
 
 };
 
-buttonView.addEventListener("click", marketingItems);
+var newbuttonPrevious = function(){
+	if(i === length -1){
+		i = -1;
+}; i++; myIntro.text = marketingItems[i];
 
-window1.add(myFirstView, buttonView, buttonView2);
-myFirstView.add(myText);
-buttonView.add(buttonText);
-buttonView2.add(buttonText2);
+};
+
+
+
+
+
+buttonNext.addEventListener("click", newbuttonNext);
+buttonPrevious.addEventListener("click", newbuttonPrevious);
+
+
+buttonNext.add(nextLabel);
+buttonPrevious.add(previousLabel);
+window1.add(buttonNext, buttonPrevious);
+window1.add(mainView);
+mainView.add(myIntro);
+buttonNext.add(nextLabel);
+buttonPrevious.add(previousLabel);
 window1.open(); 
--- Ogunjobi_Brandee_Project1/Resources/marketingItems.js
+++ Ogunjobi_Brandee_Project1/Resources/marketingItems.js
@@ -1,6 +1,5 @@
 
-
-
+var marketingItems = ["1. Web Analytics", "2. Real Time Reporting", "3. Audience Engagement", "4. Web Design", "5. Mobile Apps"];
 
 
 
